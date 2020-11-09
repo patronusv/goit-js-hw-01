a16b9ea936e2dc0c4df551ea655d0439f00dce4d
Viktor Gorokhov <patronusv@gmail.com>
2020-11-09 17:59:13 +0200

    добавляет варианты решения 6-го задания

--- js/task-6.js
+++ js/task-6.js
@@ -1,40 +1,57 @@
-let input;
+let input = '';
 let total = 0;
 let message;
 
+// ========== do while ================
+
 // do {
-//         input = prompt('Введите число', '');
-//     total += Number.parseInt(input);
-//     console.log(total);
-// } while (input != null);
-
-
-// for (let i = 0, i += 1) {
-//     input = prompt('Введите число', '');
+//      input = prompt('Введите число', '');
 //     if (input === null) {
-//         message = `Сумма равна ${total}`;
+//         message = `Сумма равна ${total}`;        
 //         break;
-//     } else if (typeof input != 'number') {
-//         alert('попробуйте еще раз');
+//     } else if (Number.isNaN(parseInt(input))) {
+//         alert('Вы ввели не число, попробуйте еще раз');
+//         input = 0;
 //     } else {
 //         total += parseInt(input);
+//          message = `Сумма равна ${total}`;        
 //     }
+// } while (input !== null);
 
-// }
+// =============== while =================
 
-do {
+// while (2 + 2 == 4) {
+//      input = prompt('Введите число', '');
+//     if (input === null) {
+//         message = `Сумма равна ${total}`;        
+//         break;
+//     } else if (Number.isNaN(parseInt(input))) {
+//         alert('Вы ввели не число, попробуйте еще раз');
+//         input = 0;
+//     } else {
+//         total += parseInt(input);
+//          message = `Сумма равна ${total}`;
+        
+//     }
+// } 
+
+// =============== for =====================
+for (let i = 0; i < 10; i++) {
      input = prompt('Введите число', '');
     if (input === null) {
-        message = `Сумма равна ${total}`;        
+        message = `Сумма равна ${total}`;
+        i = 0;
         break;
     } else if (Number.isNaN(parseInt(input))) {
         alert('Вы ввели не число, попробуйте еще раз');
         input = 0;
+        i = 0;
     } else {
         total += parseInt(input);
+        i = 0;
          message = `Сумма равна ${total}`;
         
     }
-} while (input != null);
+} 
 
 console.log(message);
